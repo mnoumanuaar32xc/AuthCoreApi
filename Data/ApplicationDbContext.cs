baa9fb56acbd6f47dd44e18882a8a8d281f4853e
Muhamad Nouman Khan <mnoumanuaar@yahoo.com>
2024-05-28 11:29:57 +0400

    Ist Check In
    Ist Check In

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using TraningWebApi.Model.Domain;
+
+namespace TraningWebApi.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+
+        public ApplicationDbContext( DbContextOptions  <ApplicationDbContext > options): base(options)
+        {
+
+        }
+
+        public DbSet<Training> Training { get; set; }
+
+
+
+
+    }
+}
---
+++ Model/Domain/Training.cs
@@ -0,0 +1,9 @@
+﻿namespace TraningWebApi.Model.Domain
+{
+    public class Training
+    {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        public  string Lavel { get; set; }
+    }
+}
