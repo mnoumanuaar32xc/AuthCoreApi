67bb784b423e42177303e11a196da13f5a611012
Muhamad Nouman Khan <mnoumanuaar@yahoo.com>
2024-05-29 14:18:54 +0400

    JWT token implementation
    JWT token implementation

---
+++ Data/AuthDbContext.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Win32;
+
+namespace TraningWebApi.Data
+{
+    public class AuthDbContext : IdentityDbContext
+    {
+        public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options)
+        {
+        }
+
+
+
+
+        // create roles and user 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // generate guid by C# Interactive command  Guid.NewGuid()
+            var readerRole = "56b1c4a5-0fa5-4713-bc05-9dfca0086082";
+            var writerRole = "15d1d7c3-9087-43be-a585-3822805f73e0";
+
+            var roles = new List<IdentityRole>
+            { new  IdentityRole()
+            {
+                Id=writerRole,
+                NormalizedName="Reader".ToUpper(),
+                ConcurrencyStamp=readerRole
+            },
+                new  IdentityRole()
+            {
+                Id=writerRole,
+                NormalizedName="Writer".ToUpper(),
+                ConcurrencyStamp=writerRole
+            }
+
+            };
+
+            //seed the roles 
+            // when the EF migration runs Roles will be seeds with it.
+
+            builder.Entity<IdentityRole>().HasData(roles);
+
+
+
+            // Create a Admin  Users which is act as Admin 
+
+            var adminUserId = "92246535-90b9-4ce2-aedd-14ac92f308be";
+
+            var admin = new IdentityUser()
+            {
+                Id = adminUserId,
+                UserName = "admin.test.com",
+                Email = "admin.test.com",
+                NormalizedEmail = "admin.test.com".ToUpper(),
+                NormalizedUserName = "admin.test.com".ToUpper()
+            };
+            // seed the admin user into Identity 
+            builder.Entity<IdentityUser>().HasData(admin);
+
+            // give Roles  to Admin 
+            // We are assign Reader and Writer both Roles
+
+
+            var adminRoles = new List<IdentityUserRole<string>>()
+            {
+
+            new(){
+            UserId=adminUserId,
+            RoleId=readerRole,
+            },
+            new(){
+
+            UserId=adminUserId,
+            RoleId=writerRole
+            }
+            };
+            // Seed the roles by users
+            builder.Entity<IdentityUserRole<string>>().HasData(adminRoles);
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using TraningWebApi.Data;
 using TraningWebApi.Repositories.Implementations;
 using TraningWebApi.Repositories.Interfaces;
@@ -15,12 +18,61 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("CodePulseConnectionString"));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("TraningDBConnectionString"));
 });
+
+builder.Services.AddDbContext<AuthDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("TraningDBConnectionString"));
+});
+
 
 // add TrainingRepository DI
 builder.Services.AddScoped<ITrainingRepository, TrainingRepository>();
+ 
+// IdentityCore
+builder.Services.AddIdentityCore<IdentityUser>()
+    .AddRoles<IdentityRole>()
+    .AddTokenProvider<DataProtectorTokenProvider<IdentityUser>>("Traning")
+    .AddEntityFrameworkStores<AuthDbContext>()
+    .AddDefaultTokenProviders();
+
+
+// Configure the Identity Options 
+
+builder.Services.Configure<IdentityOptions>(Options =>
+{
+    Options.Password.RequireDigit = false;
+    Options.Password.RequireLowercase = false;
+    Options.Password.RequireNonAlphanumeric = false;
+    Options.Password.RequireUppercase = false;
+    Options.Password.RequiredLength = 6;
+    Options.Password.RequiredUniqueChars = 1;
+
+});
+
+
+// token authentication for JW token
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            AuthenticationType = "Jwt",
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey =
+            new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+        };
+    });
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -43,6 +95,8 @@ app.UseCors(
 
 
 
+app.UseAuthentication();
+// Authentication must be befor Authorization
 app.UseAuthorization();
 
 app.MapControllers();
